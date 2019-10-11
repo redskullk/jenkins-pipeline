762a75f342dfa2d79b19870ae2e18b7bbdfecdf7
redskullk <56344108+redskullk@users.noreply.github.com>
2019-10-11 17:28:26 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
+
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class DemoApplication {
 	
